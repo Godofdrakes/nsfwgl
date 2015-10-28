d803082550ef4d68d58b682750d077749968a4b7
Urias Rooney <Urias.Rooney@gmail.com>
2015-10-28 11:12:42 -0800

    Added fix for Devend /build hanging.

--- BuildDepend.bat
+++ BuildDepend.bat
@@ -1,11 +1,12 @@
-cmake -DOPTION_BUILD_STATIC=ON -B./dep/glbinding/ -H./dep/glbinding/
-Devenv ./dep/glbinding/glbinding.sln /Build Debug /project ALL_BUILD
-Devenv ./dep/glbinding/glbinding.sln /Build Release /project ALL_BUILD
+Devenv nsfwgl.sln
 
+cmake -DOPTION_BUILD_STATIC=ON -B./dep/glbinding/ -H./dep/glbinding/
 cmake -B./dep/glfw/ -H./dep/glfw/
-Devenv ./dep/glfw/glfw.sln /Build Debug /project ALL_BUILD
-Devenv ./dep/glfw/glfw.sln /Build Release /project ALL_BUILD
 
 7z e ./dep/FBXFile.zip -o./dep/FBXFile/ -y
 
-pause
+Devenv ./dep/glbinding/glbinding.sln /Build Debug /project ALL_BUILD
+Devenv ./dep/glbinding/glbinding.sln /Build Release /project ALL_BUILD
+
+Devenv ./dep/glfw/glfw.sln /Build Debug /project ALL_BUILD
+Devenv ./dep/glfw/glfw.sln /Build Release /project ALL_BUILD

d0abf83471ef0c638bb57f1924e1e8cd840ccc4e
Urias Rooney <Urias.Rooney@gmail.com>
2015-10-28 09:41:07 -0800

    Added batch file for building all dependencies.

---
+++ BuildDepend.bat
@@ -0,0 +1,9 @@
+cmake -DOPTION_BUILD_STATIC=ON -B./dep/glbinding/ -H./dep/glbinding/
+Devenv ./dep/glbinding/glbinding.sln /Build Debug /project ALL_BUILD
+Devenv ./dep/glbinding/glbinding.sln /Build Release /project ALL_BUILD
+
+cmake -B./dep/glfw/ -H./dep/glfw/
+Devenv ./dep/glfw/glfw.sln /Build Debug /project ALL_BUILD
+Devenv ./dep/glfw/glfw.sln /Build Release /project ALL_BUILD
+
+pause

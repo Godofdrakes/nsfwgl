cddbc85918d29ba3fc9e1e829a928d98df5fad21
Urias Rooney <Urias.Rooney@gmail.com>
2015-10-29 14:37:27 -0800

    Added header for some simple colors.

---
+++ nsfwgl/Colors.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "nsfw.h"
+
+namespace nsfw {
+    namespace Colors {
+        typedef vec3 Color;
+        const Color WHITE = Color( 1.f, 1.f, 1.f );
+        const Color BLACK = Color( 0.f, 0.f, 0.f );
+        const Color RED = Color( 1.f, 0.f, 0.f );
+        const Color GREEN = Color( 0.f, 1.f, 0.f );
+        const Color BLUE = Color( 0.f, 0.f, 1.f );
+    }
+}
